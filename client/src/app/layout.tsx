4571d42c9a4e2abd2be4928bc2b10f772695dc41
thexeromin <thexeromin@gmail.com>
2026-03-28 18:59:05 +0530

    refactor: move provider folder from components to root level

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono, Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import { QueryProvider, ThemeProvider } from '@/components/providers'
+import { QueryProvider, ThemeProvider } from '@/providers'
 import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
